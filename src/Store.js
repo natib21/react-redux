3968caecbff9bedbf4fe816df9615d3e3a72da7e
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-09-13 03:46:39 -0700

    add reducer function

---
+++ src/Store.js
@@ -0,0 +1,31 @@
+import { createStore } from "redux";
+const intialVal = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const reducer = (state = intialVal, action) => {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+store.dispatch({ type: "account/deposit", payload: 50 });
+console.log(store);
