70e59ded5c7f75440cbd80ce0b762ea7c4976fce
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-09-14 11:46:44 +0300

    add react-reduxt package to connect react app to redux store

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Store from './Store.js'
-import { deposit } from './features/Accounts/AccountSlice.js'
+import {Provider} from 'react-redux'
 
-Store.dispatch(deposit(1000));
-console.log(Store.getState())
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={Store}>
+       <App />
+    </Provider>
   </StrictMode>,
 )
