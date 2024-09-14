7ece2ac3d3351d51bbd2b0a175c232c6efdecdbe
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-09-14 12:50:16 +0300

    autowire redux dispatch to react app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import CreateCustomer from "./features/Customers/CreateCustomer";
 import Customer from "./features/Customers/Customer";
 import AccountOperations from "./features/Accounts/AccountOperations";
 import BalanceDisplay from "./features/Accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector(name => name.customer.fullName)
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
+     {fullName === '' ? <CreateCustomer /> :
+     <>
       <Customer />
       <AccountOperations />
       <BalanceDisplay />
+     </>
+     }
     </div>
   );
 }
